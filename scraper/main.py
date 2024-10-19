7b1170630e53389ac88c53055ee4216f323e5dc1
StAandrew <dev.staniukynas@gmail.com>
2024-10-19 20:02:36 +0100

    Basic working version for the scraper

--- scraper/main.py
+++ scraper/main.py
@@ -1,39 +1,40 @@
 import requests
 import os
+import sqlite3
+from pydantic import BaseModel, Field
+from typing import List
+from firecrawl import FirecrawlApp
 from dotenv import load_dotenv
 
 load_dotenv()
-FIRECRAWL_KEY = os.getenv('FIRECRAWL_KEY')
+FIRECRAWL_API_KEY = os.getenv('FIRECRAWL_API_KEY')
 firecrawl_url = "https://api.firecrawl.dev/v1/scrape"
 
-base_url = "https://www.rightmove.co.uk/property-to-rent/find.html?searchLocation=Kensington+And+Chelsea+%28Royal+Borough%29&useLocationIdentifier=true&locationIdentifier=REGION%5E61229&radius=0.0&_includeLetAgreed=on&includeLetAgreed=false"
+search_url = "https://www.rightmove.co.uk/property-to-rent/find.html?searchLocation=Kensington+And+Chelsea+%28Royal+Borough%29&useLocationIdentifier=true&locationIdentifier=REGION%5E61229&radius=0.0&_includeLetAgreed=on&includeLetAgreed=false"
 
-payload = {
-    "url": "<string>",
-    "formats": ["markdown"],
-    "onlyMainContent": True,
-    "includeTags": ["<string>"],
-    "excludeTags": ["<string>"],
-    "headers": {},
-    "waitFor": 123,
-    "timeout": 123,
-    "extract": {
-        "schema": {},
-        "systemPrompt": "<string>",
-        "prompt": "<string>"
-    },
-    "actions": [
-        {
-            "type": "wait",
-            "milliseconds": 2
-        }
-    ]
-}
-headers = {
-    "Authorization": f"Bearer {FIRECRAWL_KEY}",
-    "Content-Type": "application/json"
-}
+app = FirecrawlApp(api_key=FIRECRAWL_API_KEY)
 
-response = requests.request("POST", firecrawl_url, json=payload, headers=headers)
 
-print(response.text)
+class PropertySchema(BaseModel):
+  property_id: str = Field(..., alias="property_id")
+  property_url: str = Field(..., alias="property_url")
+  property_title: str = Field(..., alias="property_title")
+  property_description: str = Field(..., alias="property_description")
+  property_address: str = Field(..., alias="property_address")
+  property_postcode_short: str = Field(..., alias="property_postcode_short")
+  property_postcode_full: str = Field(..., alias="property_postcode_full")
+  property_price_per_month: str = Field(..., alias="property_price_per_month")
+
+
+# Define a schema for the full extraction, which includes a list of properties
+class ListingsSchema(BaseModel):
+  listings: List[PropertySchema]
+
+data = app.scrape_url(search_url, {
+  'formats': ['extract'],
+  'extract': {
+    'schema': ListingsSchema.model_json_schema(),
+  }
+})
+for listing in data["extract"]["listings"]:
+    print(listing)
