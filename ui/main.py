d6548472e55e6072e7741732a4aaac04d35fdcac
Stas Chistyakov <stas@domwise.ai>
2024-10-20 20:14:57 +0100

    final UI commit

--- ui/main.py
+++ ui/main.py
@@ -11,22 +11,38 @@ st.title("Izba AI")
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 
-# User input
-user_input = st.text_input("You: ", "")
+user_input = st.text_input("You: ","")
 
-HISTORY = True
 if st.button("Send"):
     if user_input:
         # Append user input to chat history
-        st.session_state['chat_history'].append({"role": "user", "content": user_input})
+        st.session_state['chat_history'].append({"role": "user", "content": user_input, "type": "user"})
 
         orchestrator_response = requests.get(URL+f"/orchestrator/{user_input}").json()["llm_response"]
         if orchestrator_response == "out-of-scope":
             st.session_state['chat_history'].append({"role": "assistant", "content": "Unfortunately, I'm not currently capable of answering this question. Do you have any other questions about UK properties?"})    
         elif orchestrator_response.split("/")[0] == "current_listings":
-            HISTORY = False
             current_listings_data_response = requests.get(URL+f"current_listings/{user_input}").json()
-            for property in current_listings_data_response["return_data"]:
+            images = []
+            properties = current_listings_data_response["return_data"]
+            for property in properties:
+                images.append(property['image1_url'])
+            st.session_state['chat_history'].append({"role": "assistant", "content": current_listings_data_response["return_str"], "type": "current_listings", "properties": properties, "images": images})
+        else:
+            llm_response = requests.get(URL+f"/llama_index_agent/{user_input}").json()["llm_response"]
+            st.session_state['chat_history'].append({"role": "assistant", "content": llm_response, "type": "llm_agent"})
+
+# Display chat history
+for chat in st.session_state['chat_history']:
+    if chat['role'] == 'user':
+        st.markdown(f"<span style='color:green'>You</span>: {chat['content']}", unsafe_allow_html=True)
+    else:
+        if chat["type"] == "llm_agent":
+            st.markdown(f"<span style='color:red'>Domovenok</span>: {chat['content']}", unsafe_allow_html=True)
+        else:
+            st.markdown(f"<span style='color:red'>Domovenok</span>:", unsafe_allow_html=True)
+            properties = chat["properties"]
+            for property in properties:
                 with st.container():
                     cols = st.columns(2)
                     with cols[0]:
@@ -39,15 +55,5 @@ if st.button("Send"):
                         st.write(f"**Bedrooms**: {property['number_of_bedrooms']} | **Bathrooms**: {property['number_of_bathrooms']}")
                         st.write(f"**Address**: {property['address']}")
                         st.write(f"**Postcode**: {property['postcode']}")
-            st.session_state['chat_history'].append({"role": "assistant", "content": current_listings_data_response["return_str"]})
-        else:
-            llm_response = requests.get(URL+f"/llama_index_agent/{user_input}").json()["llm_response"]
-            st.session_state['chat_history'].append({"role": "assistant", "content": llm_response})
 
-# Display chat history
-if HISTORY:
-    for chat in st.session_state['chat_history']:
-        if chat['role'] == 'user':
-            st.markdown(f"<span style='color:green'>You</span>: {chat['content']}", unsafe_allow_html=True)
-        else:
-            st.markdown(f"<span style='color:red'>Domovenok</span>: {chat['content']}", unsafe_allow_html=True)
+            # st.image(chat["images"][-1], width=350)
